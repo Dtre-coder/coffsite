356a43bb499109e198d06f1e980f4e72c5836eb0
Dtre-coder <glebmaleshev@gmail.com>
2026-03-28 18:46:47 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/coffsite/',  // название твоего репозитория
   plugins: [react()],
-  base: '/coffsite/', // Это говорит сайту: "Ты живешь в папке /coffsite/"
 })
