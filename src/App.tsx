29ad593deaac37217a5c01411a8107f984ef86dd
Dtre-coder <glebmaleshev@gmail.com>
2026-03-28 13:56:41 +0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CoffeeShopPage from './pages/CoffeeShopPage';
+import ProductPage from './pages/ProductPage';
+import FavoritesPage from './pages/FavoritesPage';
+
+export default function App() {
+  return (
+    <Router basename="/coffsite">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/coffee/:id" element={<CoffeeShopPage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </Router>
+  );
+}
